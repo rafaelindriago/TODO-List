83a35ee7a9740381160df208e9deca9303f362cd
Rafael I <rafael.indriago93@gmail.com>
2024-06-26 15:40:45 -0400

    Set the description column as nullable in todos table.

--- database/migrations/2024_06_26_173750_create_todos_table.php
+++ database/migrations/2024_06_26_173750_create_todos_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('todos', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('title');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->foreignUuid('user_id')->constrained();
             $table->timestamps();
         });
