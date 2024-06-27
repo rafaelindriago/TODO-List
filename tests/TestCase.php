3717b410dc29481e3c2b248a5c251551381ac07f
Rafael I <rafael.indriago93@gmail.com>
2024-06-27 01:57:28 -0400

    Apply laravel pint.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,7 +3,6 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
